e95bee9537de24d194d9f9adba207daff0963173
redsky <hyun0238@gmail.com>
2019-08-28 17:55:55 +0900

    spm report 레이아웃작업 및 기초작업 중

--- src/containers/App.js
+++ src/containers/App.js
@@ -3,6 +3,7 @@ import {Switch, Route} from 'react-router-dom';
 
 // 화면 layout 컨테이너 컴포넌트 불러오기
 import { TopContainer, SearchContainer, NaviContainer, FooterContainer } from './layout';
+// router에 의해서 불러온 화면 컨테이너 컴포넌트 불러오기.
 import { SalesContainer } from './body';
 
 
